f7704b72b92e51542ee13aa42bc1fff610dfa26b
andigeng <hello@andigeng.com>
2017-03-04 20:18:53 -0500

    generate 10 adversarial images confusing 2 for 6

--- adversarial_generator.py
+++ adversarial_generator.py
@@ -60,11 +60,10 @@ def main():
     with sess.as_default():
         tf.global_variables_initializer().run()
         load_model(utils.SAVE_DIR)
-        idx_arr = find_image_indices(2, 3)
-        for idx in idx_arr:
-            image, _ = get_test_element(idx)
-            args = generate_adversarial_image(image, 6, max_steps=10000)
-            plot_comparison(*args)
+        example_arr = generate_adversarial_images(original_num=2, 
+                                                  target_num=6,
+                                                  quantity=10)
+        plot_multiple_comparisons(*example_arr)
     sess.close()
 
 
@@ -76,52 +75,56 @@ def load_model(directory):
     print("Model loaded.")
 
 
-def generate_adversarial_image(image, target, max_steps=10000, change_rate=1.0/4000, max_noise=0.25):
-    """ Given an image and target, tries to create an image that will be misclassified to target.
-    Input: 
-        image: a numpy array of length 784 representing a 28x28 image
-        target: integer
-    Output:
-        3 numpy arrays of length 784 representing the original image, noise, and adversarial image
+def generate_adversarial_images(original_num, target_num, quantity, max_steps=10000):
+    """ 
+    Creates and returns three lists of images: original image, noise, and the adversarial image
+    """
+    idx_arr = find_image_indices(original_num, quantity)
+    image_arr, noise_arr, adversarial_arr = [], [], []
+    
+    for idx in idx_arr:
+        image, _ = get_test_element(idx)
+        _, noise, adversarial = generate_adversarial_image(image, target_num, max_steps)
+        image_arr.append(image)
+        noise_arr.append(noise)
+        adversarial_arr.append(adversarial)
+
+    saved_arr = np.array(image_arr + noise_arr + adversarial_arr)
+    np.save('adversarial_grid', saved_arr)
+    return image_arr, noise_arr, adversarial_arr
+
+
+def generate_adversarial_image(image, target, max_steps, change_rate=1.0/4000):
+    """ 
+    Given an image and target, tries to create an image that will be misclassified to target.
     """
     label = get_target_label(target)
     noise = np.zeros((1, 784))
     # Generates gradients with respect to loss function and the input image.
     gradient = tf.gradients(cross_entropy, [x])[0]
-    # This will hold the value of a gradient after being evaluated
-    gradient_value = np.zeros((1, 784))
 
     for step in range(max_steps):
-        # The adversarial image is the original image plus the noise image
         adversarial_image = image + noise
         # We clip the values so that they are between 0 and 1
         np.clip(adversarial_image, 0.0, 1.0, out=adversarial_image)
+        noise = adversarial_image - image
         # Evaluate the gradient of the adversarial input with respect to our loss
-        gradient_value = sess.run(gradient, feed_dict={x: adversarial_image, y_: label, keep_prob:1.0})
 
-        if step%100 == 0: 
-            print(step)
-        if prediction.eval(feed_dict={x:adversarial_image, keep_prob:1.0}) == target: 
-            print(step)
+        feed_dict = {x: adversarial_image, y_: label, keep_prob: 1.0}
+        gradient_value = sess.run(gradient, feed_dict)
+        if is_adversarial(adversarial_image, target):
+            print("Adversarial image found. {} steps taken.".format(step))
             break
-        # We subtract the gradient from the noise. This ensures that on the next pass, the 
-        # chance of the model being fooled will increase.
-        noise -= gradient_value*change_rate
-        np.clip(noise, 0.0, max_noise, out=noise)
+        noise -= gradient_value * change_rate
 
-    new_prediction = prediction.eval(feed_dict={x:adversarial_image, keep_prob:1.0})[0]
-    
-    if new_prediction != target:
+    if not is_adversarial(adversarial_image, target):
         print("No adversarial image found. Try increasing max_steps or change_rate.")
 
-    return image, adversarial_image-image, adversarial_image
+    return image, noise, adversarial_image
 
 
 def find_image_indices(target, quantity):
     """ Given a target label, and quantity, tries to find elements that match the criteria.
-    Input:
-        target: integer from 0-9 representing what types of elements you want
-        quantity: integer for how many you want
     Output:
         target_indices: list of indices representing the positions of the elements found
     """
@@ -134,62 +137,43 @@ def find_image_indices(target, quantity):
     return target_indices
 
 
+def plot_multiple_comparisons(image_arr, noise_arr, adversarial_arr):
+    """ Given three lists, plots and saves a n x 3 image
+    """
+    huge_arr = image_arr + noise_arr + adversarial_arr
+    num_images = len(image_arr)
+    figure, axes = plt.subplots(num_images, 3)
+    figure.set_size_inches(3, num_images, forward=True)
+    
+    for row, ax in enumerate(axes):
+        for col, sub_ax in enumerate(ax):
+            image_to_plot = huge_arr[col*num_images + row].reshape((28,28))
+            sub_ax.imshow(image_to_plot, 
+                          cmap='gray_r', vmin=0.0, vmax=1.0, interpolation='nearest')
+            sub_ax.set_aspect("auto")
+            sub_ax.set_xticks([])
+            sub_ax.set_yticks([])
+    figure.subplots_adjust(hspace=0, wspace=0, left=0, right=1, bottom=0, top=1)
+    plt.savefig('adversarial_grid', dpi=112)
+    plt.show()
+
+
 def get_test_element(idx):
+    """ Return the image and label at a given index within the test set. """
     image = mnist.test.images[idx].reshape((-1, 784))
     label = mnist.test.labels[idx].reshape((-1, 10))
     return image, label
 
 
 def get_target_label(num):
+    """ Returns the specified hot-encoded label, a numpy array. """
     label = np.zeros([1,10])
     label[0][num] = 1.0
     return label
 
 
-def plot_image(image, label, prediction):
-    """ Plots an image with corresponding label and prediction.
-    Input: 
-        image: length 784 numpy array, representing a 28x28 imagez
-        label: integer
-        prediction: integer
-    """
-    print('plotting')
-    figure, axes = plt.subplots()
-    image = image.reshape((28,28))
-    axes.imshow(image, cmap='gray_r', vmin=0.0, vmax=1.0)
-    axes.set_xlabel("Actual: {}, Predicted: {}".format(label, prediction))
-    axes.set_xticks([])
-    axes.set_yticks([])
-    plt.show()
-
-
-def plot_comparison(image, noise, adversarial_image):
-    """ Plots the original image, noise, and adversarial image side by side.
-    Input: 
-        3 numpy arrays of size 784, each representing a 28x28 image 
-    """
-    plots = [(image, 'original'), (noise, 'noise'), (adversarial_image, 'adversarial')]
-    figure, axes = plt.subplots(1,3)
-    for idx, ax in enumerate(axes):
-        image_to_plot = plots[idx][0].reshape((28,28))
-        ax.imshow(image_to_plot, cmap='gray_r', vmin=0.0, vmax=1.0)
-        ax.set_xlabel("{} image".format(plots[idx][1]))
-        ax.set_xticks([])
-        ax.set_yticks([])
-    plt.show()
-
-
-def testing_accuracy(to_print=True):
-    """ Run through the test set and return the accuracy, with an option to print. """
-    feed_dict = {x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}
-    return sample_accuracy(feed_dict, to_print=to_print, test_type='test') 
-
-
-def sample_accuracy(feed_dict, to_print=True, test_type='training'):
-    """ Run through the given set and return the accuracy, with an option to print. """
-    accuracy_ratio = accuracy.eval(feed_dict)
-    if to_print: print("{} accuracy {}".format(test_type, accuracy_ratio))
-    return accuracy_ratio
+def is_adversarial(image, target):
+    return prediction.eval(feed_dict={x:image, keep_prob:1.0}) == target
 
 
 if __name__ == '__main__':
